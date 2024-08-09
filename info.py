19d24e5e55e42eb5fb795a2d922e053a995a82bb
umar2111k <138141263+umar2111k@users.noreply.github.com>
2024-08-09 17:54:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,7 +97,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', True))
+VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'vipurl.in')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '5a46477839bb186f9d168d34295a0db50eae2f05')
 # if verify second shortner is True then fill below url and api
